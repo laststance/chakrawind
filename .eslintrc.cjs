f23c184a8b5e16bd4666685867eabd90773caecb
Ryota Murakami <dojce1048@gmail.com>
2026-03-05 23:00:29 +0900

    feat(m0): bootstrap workspace skeleton

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist", "node_modules"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
+    ]
+  }
+}
